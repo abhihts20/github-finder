f062e150a71914a9bb339ba7ea5cd0c607265190
Abhinav Varshney <abhihts20@gmail.com>
2024-10-02 13:42:58 +0530

    fix: key for github in env

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -1,19 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { enviroment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProfileService {
   baseUrl: string = 'https://api.github.com/users';
+  githubKey = enviroment.githubKey;
   constructor(private http: HttpClient) {}
 
   getUserProfile(userName: string): Observable<any> {
     return this.http.get(`${this.baseUrl}/${userName}`, {
       headers: {
         Accept: 'application/vnd.github+json',
-        Authorization: 'Bearer',
+        Authorization: `Bearer ${this.githubKey}`,
         'X-GitHub-Api-Version': '2022-11-28',
       },
     });
